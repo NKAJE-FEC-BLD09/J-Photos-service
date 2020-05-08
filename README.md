6012fcf8d871bd5e102d25ad46037a38fe2e39da
Jackson Reinagel <jreinagel4@gmail.com>
2020-05-07 22:12:11 -0600

    Add working db seed script and combined setup script

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # J-Photos-service
 Photos module
+
+To initialize app, use "npm run setup" to spin up client and server and seed mongoDB.
---
+++ server/resources/seedDB.js
@@ -0,0 +1,30 @@
+const Photo = require('./Photo');
+const db = require('../db');
+const fs = require('fs');
+const csv = require('csv-parser');
+const faker = require('faker');
+
+const photos = [];
+
+const insertAllPhotos = () => {
+  fs.createReadStream('./server/resources/PhotoDB.csv')
+    .pipe(csv())
+    .on('data', (row) => {
+      photos.push(row);
+    })
+    .on('end', () => {
+      Photo.insertMany(photos, (err, docs) => {
+        if (err) throw err;
+        if (docs) {
+          for (let n = 47; n > 0; n--) {
+            Photo.updateOne({ caption: '' }, { caption: faker.random.words(7) }, (err, photo) => {
+              if (err) console.log(err);
+              if (photo) console.log(photo);
+            });
+          }
+        }
+      });
+    });
+};
+
+insertAllPhotos();
