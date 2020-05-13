5a51bc9d2e418a5f3382453d224e486c3cab97a8
Jackson Reinagel <jreinagel4@gmail.com>
2020-05-13 17:13:36 -0600

    Add arrows, title and other buttons and banners to modal

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Photo from './Photo';
 
 const photoDB = require('../../../data/photodb.json');
 const movieDB = require('../../../data/moviedb.json');
@@ -8,12 +9,11 @@ class Modal extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      currentPic: props.currentPic,
-      currentFlick: props.currentFlick,
-      pics: props.pics
-    }
-    this.onClose = this.onClose.bind(this);
+    // this.state = {
+    //   currentPic: props.currentPic,
+    //   currentFlick: props.currentFlick,
+    //   pics: props.pics
+    // }
   }
 // const Modal = (props) => {
 
@@ -27,13 +27,33 @@ class Modal extends React.Component {
     }
     return (
       <div className='modal'>
-        <div className='ad banner'></div>
+        <div className='ad-banner'><span className='ad'></span></div>
+        <div className='gallery-header'>
+          <a className='gallery-link' href={this.props.currentFlick.link}>
+            <span className='grid-btn'></span>
+            <span className='gallery-title'>{this.props.currentFlick.title} ({this.props.currentFlick.copyright})</span>
+          </a>
+          <button className='close' onClick={this.onClose} aria-label="Close"></button>
+        </div>
         <div className='modal-main'>
-          <div className='gallery-header'>
-            <button className='grid-btn'></button>
-            <button className='close' onClick={this.onClose} aria-label="Close"></button>
+          <button className='left arrow-btn'>
+            ::before
+          </button>
+          <Photo photo={this.props.currentPic} />
+          {/* <img className='gallery-object' title={this.props.currentPic.caption} src={this.props.currentPic.url} /> */}
+          <button className='right arrow-btn'>
+            ::before
+          </button>
+        </div>
+        <div className='gallery-footer'>
+          <div className='image-info'>
+            <div className='top-row'>
+              <div className='btn-links'>
+                <a className='link-btn' href="https://www.imdb.com/registration/signin">Edit Tags</a>
+                <a className='link-btn' href="https://www.imdb.com/registration/signin">Report This</a>
+              </div>
+            </div>
           </div>
-          <img className='gallery-object' title={this.state.currentPic.caption} src={this.state.currentPic.url} />
         </div>
       </div>
     );
--- client/src/components/Photo.jsx
+++ client/src/components/Photo.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 var Photo = ({photo, handlePicClick}) => (
   <span className='media'>
     <a target='_blank'>
-      <img className='media-object' height='99' width='99' title={photo.caption} src={photo.url} onClick={handlePicClick} />
+      <img className='media-object' title={photo.caption} src={photo.url} onClick={handlePicClick} />
     </a>
   </span>
 );
