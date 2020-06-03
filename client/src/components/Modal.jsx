c5402f4df530f6a3c70b5499669a4caedb0b5fc9
jackrein <jreinagel4@gmail.com>
2020-06-02 18:17:13 -0600

    Split up scss into 2 files; add vanishing features

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
-const photoDB = require('../../../data/photodb.json');
-const movieDB = require('../../../data/moviedb.json');
+import '../styles/modal.scss';
 
 class Modal extends React.Component {
   constructor(props) {
@@ -28,46 +26,50 @@ class Modal extends React.Component {
     return (
       <div className='modal'>
         <div className='ad-banner'><a className='ad' href='https://www.amazon.com/Bosch-Season-6-Official-Trailer/dp/B085JXLMHQ'></a></div>
-        <div className='gallery-header'>
-          <a className='gallery-link' href={this.props.currentFlick.link}>
-            <span className='grid-btn btn'></span>
-            <span className='gallery-title'>{this.props.currentFlick.title} ({this.props.currentFlick.copyright})</span>
-          </a>
-          <button className='close btn' onClick={this.onClose} aria-label="Close"></button>
-        </div>
         <div className='modal-main'>
-          <button className='left arrow-btn btn' onClick={this.leftClick}>
-            ::before
-          </button>
           <img className='gallery-object' id={this.props.currentPic.photo_id} src={this.props.currentPic.url} />
-          <button className='right arrow-btn btn' onClick={this.rightClick}>
-            ::before
-          </button>
+          <div className='disappearing-btn'>
+            <button className='left arrow-btn btn' onClick={this.leftClick}>
+              ::before
+            </button>
+            <button className='right arrow-btn btn' onClick={this.rightClick}>
+              ::before
+            </button>
+          </div>
         </div>
-        <div className='gallery-footer'>
-          <div className='image-info'>
-            <div className='top-row'>
-              <div className='image-count'>{this.props.pics.indexOf(this.props.currentPic) + 1} of {this.props.pics.length}</div>
-              <div className='btn-links'>
-                <a className='link-btn btn' href="https://www.imdb.com/registration/signin">Edit Tags</a>
-                <a className='link-btn btn' href="https://www.imdb.com/registration/signin">Report This</a>
+        <div className='disappearing-banners'>
+          <div className='gallery-header'>
+            <a className='gallery-link' href={this.props.currentFlick.link}>
+              <span className='grid-btn btn'></span>
+              <span className='gallery-title'>{this.props.currentFlick.title} ({this.props.currentFlick.copyright})</span>
+            </a>
+            <button className='close btn' onClick={this.onClose} aria-label="Close"></button>
+          </div>
+          <div className='gallery-footer'>
+            <div className='image-info'>
+              <div className='top-row'>
+                <div className='image-count'>{this.props.pics.indexOf(this.props.currentPic) + 1} of {this.props.pics.length}</div>
+                <div className='btn-links'>
+                  <a className='link-btn btn' href="https://www.imdb.com/registration/signin">Edit Tags</a>
+                  <a className='link-btn btn' href="https://www.imdb.com/registration/signin">Report This</a>
+                </div>
               </div>
-            </div>
-            <p className='image-links'>
-              <a href={this.props.currentPic.links}>{this.props.currentPic.people}</a>
-              &nbsp;in&nbsp;
-              <a href={this.props.currentFlick.link}>{this.props.currentFlick.title} ({this.props.currentFlick.copyright})</a>
-            </p>
-            <div className='metadata'>
-              <div className='title-link'>
-                <strong>Titles : </strong>
-                <a href={this.props.currentFlick.link}>{this.props.currentFlick.title}</a>
-              </div>
-              <div className='actor-links'>
-                <strong>People : </strong>
+              <p className='image-links'>
                 <a href={this.props.currentPic.links}>{this.props.currentPic.people}</a>
+                &nbsp;in&nbsp;
+                <a href={this.props.currentFlick.link}>{this.props.currentFlick.title} ({this.props.currentFlick.copyright})</a>
+              </p>
+              <div className='metadata'>
+                <div className='title-link'>
+                  <strong>Titles : </strong>
+                  <a href={this.props.currentFlick.link}>{this.props.currentFlick.title}</a>
+                </div>
+                <div className='actor-links'>
+                  <strong>People : </strong>
+                  <a href={this.props.currentPic.links}>{this.props.currentPic.people}</a>
+                </div>
+                <div className='copyright'>© {this.props.currentFlick.copyright} {this.props.currentFlick.studio}</div>
               </div>
-              <div className='copyright'>© {this.props.currentFlick.copyright} {this.props.currentFlick.studio}</div>
             </div>
           </div>
         </div>
